a66cbfa0ea6ff826c5e627c8c2b782cab7d0adda
tarrsan2024-wq <tarrsan.2024@vitstudent.ac.in>
2026-09-23 16:40:02 +0530

    jenkins cahnged

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ def multiply(a, b):
     return a*b
 
 def divide(a,b):
-    if a==0 or b ==0:
-        return "ZeroDivisionError"
     return a/b
--- test_app.py
+++ test_app.py
@@ -6,5 +6,3 @@ def test_multiply():
 
 def test_divide():
     assert divide(10, 2) == 5
-    with pytest.raises(ValueError):
-        divide(10, 0)
